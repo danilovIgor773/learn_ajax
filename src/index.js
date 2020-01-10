cef87f8d18f34d3d0d80f74b64d65ac1340c205d
Danilov Igor <danilovigor773@gmail.com>
2020-01-10 14:26:01 +0500

    Added axios global interceptors, and some global stuff for baseURL and headers.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+
+//It is also possible to set defaults using axios for url to be used in http/ specify headers, content-type etc...
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+axios.interceptors.request.use(config => {
+    console.log(config);    
+    return config;
+},
+    error => {
+        console.log('[Error]',error); //Rises when there is no internet connection
+        return Promise.reject(error)
+    }
+)
+
+axios.interceptors.response.use(config => {
+    console.log(config);
+    return config;    
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+})
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
