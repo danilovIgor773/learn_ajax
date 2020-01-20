833e77a057e3e6e53a519858ff302deba7b42c47
Danilov Igor <danilovigor773@gmail.com>
2020-01-20 16:24:12 +0500

    Added two routes with 'Link' component to preserve state of app not to be reloaded every time

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import {Route, Link } from 'react-router-dom';
 
 import Posts from '../Blog/Posts/Posts';
+import NewPost from '../Blog/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -14,11 +16,18 @@ class Blog extends Component {
             <div className="Blog">
                 <nav>
                     <ul>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/new-post">New Post</a></li>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to={{
+                            pathname: 'new-post',
+                            hash: '#submit',
+                            search: '?param=1'
+                        }}>New Post</Link></li>
                     </ul>
                 </nav>
-                <Posts />             
+                {/* {<Route path='/' exact render={() => <h1>Home test!</h1>}></Route>
+                <Route path='/' render={() => <h1>Home test2!</h1>}></Route>} */}
+                <Route path='/' exact component={Posts}></Route>
+                <Route path='/new-post' component={NewPost}></Route>
             </div>
         );
     }
