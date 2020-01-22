89655297536cafc62974b8c614633d06641a9a93
Danilov Igor <danilovigor773@gmail.com>
2020-01-22 16:07:41 +0500

    Added hoc for code-splitting or lazy-loading.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import {Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import Posts from '../Blog/Posts/Posts';
-import NewPost from '../Blog/NewPost/NewPost';
+//import NewPost from '../Blog/NewPost/NewPost';
 import './Blog.css';
+import asyncComponent from '../../components/hoc/asyncComponent';
+
+//Here we use our async hoc to render our component dynamically by webpack in a separate chunk.js file
+const asyncNewPost = asyncComponent(() => {
+    return import('../Blog/NewPost/NewPost');
+})
+
 
 class Blog extends Component {
     state = {
-        auth: false
+        auth: true
     }
 
     render () {       
@@ -42,7 +49,7 @@ class Blog extends Component {
                 {/* {<Route path='/' exact render={() => <h1>Home test!</h1>}></Route >
                 <Route path='/' render={() => <h1>Home test2!</h1>}></Route>} */}
                 <Switch>                    
-                    {this.state.auth ? <Route path='/new-post' component={NewPost} /> : null}
+                    {this.state.auth ? <Route path='/new-post' component={asyncNewPost} /> : null}
                     <Route path='/posts' component={Posts}></Route> 
                     {/* {<Redirect from='/' to="/posts"/>}            */}
                     <Route render={() => <h1 style={{width: '100%', textAlign: 'center'}}>404 Not Found</h1>} /> 
